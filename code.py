99aa85b3ffd781db414e4c539ed53c647a95cce9
wmj998 <wmj998@foxmail.com>
2021-05-26 09:28:25 +0800

    Update code

--- code.py
+++ code.py
@@ -1,176 +1,82 @@
 import requests
-import pymysql
+import pandas as pd
 import matplotlib.pyplot as plt
+from relation_mysql import action_db
+from pd_mysql import action_table
 
-# 获取数据
-def getHTMLText(url):
+
+def get_html(url):
     try:
-        data = requests.get(url)
-        print("获取数据成功")
-        return data.text
+        response = requests.get(url=url)
+        response.raise_for_status()
+        response.encoding = response.apparent_encoding
+        return response.json()
     except:
-        print("Error-getHTMLText")
-        return ""
+        print('Error_get_html')
+        return " "
 
-# 存储数据
-def addData(information):
-    try:
-        con = pymysql.Connect("localhost", "root", "123456", "gp")  # 连接mysql（用户名，密码，库名）
-        cur = con.cursor()  # 获取游标
-        cur.execute("insert into shares(day,open,high,low,close,volume) values(%s,%s,%s,%s,%s,%s)", information)
-        con.commit()  # 提交
-        print("存储数据成功")
-    except:
-        print("Error-addData")
-        return ""
 
-# 处理数据
-def getData(html,num):
-    try:
-        alls = []
-        for i in range(0, 6 * num):  # 属性个数* 数量    提取数据
-            all = html.split('"')[3 + 4 * i]
-            alls.append(all)
+def get_data(infos):
+    data = []
+    for info in infos:
+        columns = info.keys()
+        data.append(info.values())
+    data = pd.DataFrame(data=data, columns=columns)
+    return data
 
-        for i in range(0, num):  # 分隔数据
-            datas = []
-            for j in range(0, 6):
-                data = alls[j + i * 6]
-                datas.append(data)
-            addData(datas)
-        print("处理数据成功")
-    except:
-        print("Error-getData")
-        return ""
 
-# 导出数据
-def takeData():
-    try:
-        con = pymysql.Connect("localhost", "root", "123456", "gp")  # 连接mysql
-        cur = con.cursor()  # 获取游标
-        cur.execute("select * from shares")  # ""引号内sql查询语句
-        print("导出数据成功")
-        return cur.fetchall()  # 获取记录集
-    except:
-        print("Error-takeData")
-        return ""
+def create_db(db):
+    action = action_db(host="localhost", user="root",
+                       password="w_f1216570180", port=3306)
+    action.create(db=db)
 
-# 解析数据
-def processing(rs):
-    try:
-        x = []  # day
-        y1 = []  # open
-        y2 = []  # high
-        y3 = []  # low
-        y4 = []  # close
-        y5 = []  # volume
-        for one in rs:
-            x.append(one[0])
-            y1.append(float(one[1]))
-            y2.append(float(one[2]))
-            y3.append(float(one[3]))
-            y4.append(float(one[4]))
-            y5.append(float(one[5]))
-        print("解析数据成功")
-        return x, y1, y2, y3, y4, y5
-    except:
-        print("Error-processing")
-        return ""
 
-# 数据预测
-def forecast(x, y1, y2, y3, y4, y5):
-    try:
-        df = pd.DataFrame([y1, y2, y3, y4, y5], index=['open', 'high', 'low', 'close', 'volume'], columns=x)
+class Data:
+    def __init__(self):
+        self.action = action_table(user='root', password='w_f1216570180',
+                                   host='localhost', port=3306, db='stock')
 
-        train_x = df[x[:int(len(x) / 2) - 1]]
-        train_y = df[[x[int(len(x) / 2) - 1]]]
-        test_x = df[x[int(len(x) / 2):len(x) - 1]]
+    def save_data(self, table, data):
+        self.action.save(table=table, data=data)
 
-        x_train = np.array(train_x)
-        y_train = np.array(train_y)
-        x_test = np.array(test_x)
+    def read_data(self, table):
+        data = self.action.read(table)
+        return data
 
-        # knn拟合预测
-        # knn = KNeighborsClassifier(n_neighbors=6)
-        # knn.fit(x_train, y_train.astype('str'))
-        # y_test = knn.predict(x_test)
 
-        # svc拟合预测
-        # svc = SVC(kernel='rbf', probability=True)
-        # svc.fit(x_train, y_train.astype('str'))
-        # y_test = svc.predict(x_test)
+class Df:
+    def __init__(self, data):
+        data['day'] = pd.to_datetime(data['day'])
+        data = data.set_index('day')
+        self.data = data.astype(float)
 
-        # 感知器分类
-        # ppn = Perceptron()
-        # ppn.fit(x_train, y_train.astype('str'))
-        # y_test = ppn.predict(x_test)
-
-        # logistic分类
-        # lr = LogisticRegression(C=1000.0, random_state=0)
-        # lr.fit(x_train, y_train.astype('str'))
-        # y_test = lr.predict(x_test)
-
-        # 决策树分类
-        # tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
-        # tree.fit(x_train, y_train.astype('str'))
-        # y_test = tree.predict(x_test)
-
-        # 随机森林分类
-        forest = RandomForestClassifier(criterion='entropy', n_estimators=10, random_state=1, n_jobs=2)
-        forest.fit(x_train, y_train.astype('str'))
-        y_test = forest.predict(x_test)
-
-        print('open:', y_test[0], '\nhigh:', y_test[1], '\nlow:', y_test[2], '\nclose:', y_test[3], '\nvolume:',y_test[4])
-    except:
-        print("Error-forecast")
-        return ""
-
-# 绘图
-def Drawing(x, y1, y2, y3, y4, y5):
-    try:
-
+    def visual(self):
         plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决plt中文显示的问题
         plt.rcParams['axes.unicode_minus'] = False  # 解决plt负号显示的问题
-
-        plt.title("股票价格波动趋势")
-        plt.xlabel("时间")
-        plt.ylabel("价格")
-
-        # plt.scatter(x, y1)  # 散点图
-        # plt.scatter(x, y2)  # 散点图
-        # plt.scatter(x, y3)  # 散点图
-        # plt.scatter(x, y4)  # 散点图
-
-        plt.plot(x, y1, label="open", linewidth=1, linestyle=':')   # 折线图
-        plt.plot(x, y2, label="high", linewidth=1, linestyle='-.')  # 折线图
-        plt.plot(x, y3, label="low", linewidth=1, linestyle='--')   # 折线图
-        plt.plot(x, y4, label="close", linewidth=1, linestyle='-')  # 折线图
-        plt.legend()  # 图例
+        linestyle = ['-.', '--', '-', ':']
+        self.data[['open', 'high', 'low', 'close']].plot(title='股票价格波动趋势',
+                                                         ylabel='prices', style=linestyle)
+        self.data[['volume']].plot(title='股票成交量波动趋势', ylabel='numbers')
         plt.show()
 
-        plt.title("股票volume波动图")
-        plt.xlabel("时间")
-        plt.ylabel("volume")
-        plt.plot(x, y5, label="close")  # 折线图
-        plt.legend()  # 图例
-        plt.show()
+    # def predict(self):
+    #     model = Prophet().fit(self.data)
+    #     future = model.make_future_dataframe(freq='D', periods=365)
+    #     forecast = model.predict(future)
+    #     return forecast
 
-        print("绘图成功")
-    except:
-        print("Error-Drawing")
-        return ""
 
 def main():
-    num = int(input("请输入数量："))
-    time = input("请输入时间间隔（分钟）：")  # 30或者60
-    url = "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=sh600438&scale=" + time + "&ma=no&datalen=" + str(num)
-    html = getHTMLText(url)
-    getData(html,num)
-    rs = takeData()
-    Tuple = processing(rs)
-    L = list(Tuple)
-    forecast(L[0],L[1],L[2],L[3],L[4],L[5])
-    Drawing(L[0],L[1],L[2],L[3],L[4],L[5])
+    url = 'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/' \
+          'CN_MarketData.getKLineData?symbol=sh600438&scale=60&ma=no&datalen=200'
+    infos = get_html(url)
+    data = get_data(infos)
+    create_db(db='stock')
+    Data().save_data(table='prices', data=data)
+    data = Data().read_data(table='prices')
+    df = Df(data)
+    df.visual()
+
 
 if __name__ == "__main__":
     main()
